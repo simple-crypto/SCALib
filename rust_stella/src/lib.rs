80cbaf16c81353c9fd6a512c1908b1ab9e21c343
obronchain <olivier.bronchain@uclouvain.be>
2020-02-18 10:03:27 +0100

    optimize RUST SNR function

--- rust_stella/src/lib.rs
+++ rust_stella/src/lib.rs
@@ -1,48 +1,17 @@
-use ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
-use numpy::{IntoPyArray, PyArrayDyn};
-use pyo3::prelude::{pymodule, Py, PyModule, PyResult, Python};
+use ndarray::s;
+use numpy::PyArrayDyn;
+use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
 
 #[pymodule]
 fn rust_stella(_py: Python, m: &PyModule) -> PyResult<()> {
-    // immutable example
-    fn axpy(a: f64, x: ArrayViewD<f64>, y: ArrayViewD<f64>) -> ArrayD<f64> {
-        a * &x + &y
-    }
-
-    // mutable example (no return)
-    fn mult(a: f64, mut x: ArrayViewMutD<f64>) {
-        x *= a;
-    }
-
-    // wrapper of `axpy`
-    #[pyfn(m, "axpy")]
-    fn axpy_py(
-        py: Python,
-        a: f64,
-        x: &PyArrayDyn<f64>,
-        y: &PyArrayDyn<f64>,
-    ) -> Py<PyArrayDyn<f64>> {
-        let x = x.as_array();
-        let y = y.as_array();
-        axpy(a, x, y).into_pyarray(py).to_owned()
-    }
-
-    // wrapper of `mult`
-    #[pyfn(m, "mult")]
-    fn mult_py(_py: Python, a: f64, x: &PyArrayDyn<f64>) -> PyResult<()> {
-        let x = x.as_array_mut();
-        mult(a, x);
-        Ok(())
-    }
-
     #[pyfn(m, "update_snr")]
     fn update_snr(
         _py: Python,
-        traces: &PyArrayDyn<i16>,
-        x: &PyArrayDyn<u16>,
-        sum: &mut PyArrayDyn<i64>,
-        sum2: &mut PyArrayDyn<i64>,
-        ns: &mut PyArrayDyn<u32>,
+        traces: &PyArrayDyn<i16>,   // (len,N_sample)
+        x: &PyArrayDyn<u16>,        // (Np,len)
+        sum: &mut PyArrayDyn<i64>,  // (Np,Nc,N_sample)
+        sum2: &mut PyArrayDyn<i64>, // (Np,Nc,N_sample)
+        ns: &mut PyArrayDyn<u32>,   // (Np,Nc)
     ) -> PyResult<()> {
         let traces = traces.as_array();
         let x = x.as_array();
@@ -51,17 +20,17 @@ fn rust_stella(_py: Python, m: &PyModule) -> PyResult<()> {
         let mut ns = ns.as_array_mut();
 
         let n_traces = traces.shape()[0];
-        let len = traces.shape()[1];
+        //let len = traces.shape()[1];
         let np = sum.shape()[0];
         for i in 0..n_traces {
-            for t in 0..len {
-                let l = traces[[i, t]] as i32;
-                let l2 = l * l;
-                for p in 0..np {
-                    let v = x[[p, i]] as usize;
-                    sum[[p, v, t]] += l as i64;
-                    sum2[[p, v, t]] += l2 as i64;
-                }
+            let l = traces.slice(s![i, ..]).mapv(|x| x as i64);
+            let l2 = l.mapv(|x| x * x);
+            for p in 0..np {
+                let v = x[[p, i]] as usize;
+                let mut sl = sum.slice_mut(s![p, v, ..]); //(N_sample)
+                sl += &l.slice(s![..]);
+                let mut sl = sum2.slice_mut(s![p, v, ..]); //(N_sample)
+                sl += &l2.slice(s![..]);
             }
             for p in 0..np {
                 let v = x[[p, i]] as usize;
