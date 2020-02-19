46b73916efa2da442081157234a767538597d240
obronchain <olivier.bronchain@uclouvain.be>
2020-02-19 10:39:27 +0100

    having a look at asm out of rust

--- benchmark/benchmark_snr.py
+++ benchmark/benchmark_snr.py
@@ -3,8 +3,8 @@ import numpy as np
 import re
 
 Nc = 16
-Np = 4
-l = 10000
+Np = 8
+l = 50000
 n = 10000
 def test_SNR_RUST(benchmark):
     traces = np.random.randint(0,256,(n,l),dtype=np.int16)
@@ -16,3 +16,8 @@ def test_SNR_noRUST(benchmark):
     x = np.random.randint(0,Nc,(Np,l),dtype=np.int16)
     snr = SNR(Nc,l,Np)
     benchmark(snr.fit_u,traces,x,use_rust=False)
+if __name__ == "__main__":
+    traces = np.random.randint(0,256,(n,l),dtype=np.int16)
+    x = np.random.randint(0,Nc,(Np,l),dtype=np.int16)
+    snr = SNR(Nc,l,Np)
+    snr.fit_u(traces,x,use_rust=True)
--- rust_stella/src/lib.rs
+++ rust_stella/src/lib.rs
@@ -1,17 +1,16 @@
 use ndarray::s;
-use numpy::PyArrayDyn;
+use numpy::{PyArray2, PyArray3};
 use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
-
 #[pymodule]
 fn rust_stella(_py: Python, m: &PyModule) -> PyResult<()> {
     #[pyfn(m, "update_snr")]
     fn update_snr(
         _py: Python,
-        traces: &PyArrayDyn<i16>,   // (len,N_sample)
-        x: &PyArrayDyn<u16>,        // (Np,len)
-        sum: &mut PyArrayDyn<i64>,  // (Np,Nc,N_sample)
-        sum2: &mut PyArrayDyn<i64>, // (Np,Nc,N_sample)
-        ns: &mut PyArrayDyn<u32>,   // (Np,Nc)
+        traces: &PyArray2<i16>,   // (len,N_sample)
+        x: &PyArray2<u16>,        // (Np,len)
+        sum: &mut PyArray3<i64>,  // (Np,Nc,N_sample)
+        sum2: &mut PyArray3<i64>, // (Np,Nc,N_sample)
+        ns: &mut PyArray2<u32>,   // (Np,Nc)
     ) -> PyResult<()> {
         let traces = traces.as_array();
         let x = x.as_array();
@@ -20,34 +19,35 @@ fn rust_stella(_py: Python, m: &PyModule) -> PyResult<()> {
         let mut ns = ns.as_array_mut();
 
         let n_traces = traces.shape()[0];
-        //let len = traces.shape()[1];
-        let np = sum.shape()[0];
-        for i in 0..n_traces {
-            let l = traces.slice(s![i, ..]).mapv(|x| x as i64);
-            let l2 = l.mapv(|x| x * x);
-            for p in 0..np {
+        let np = sum.shape()[0] as usize;
+
+        for p in 0..np {
+            for i in 0..n_traces {
                 let v = x[[p, i]] as usize;
-                let mut sl = sum.slice_mut(s![p, v, ..]); //(N_sample)
-                sl += &l.slice(s![..]);
-                let mut sl = sum2.slice_mut(s![p, v, ..]); //(N_sample)
-                sl += &l2.slice(s![..]);
-            }
-            for p in 0..np {
-                let v = x[[p, i]] as usize;
+                let mut m = sum.slice_mut(s![p, v, ..]);
+                let mut sq = sum2.slice_mut(s![p, v, ..]);
+                let l = traces.slice(s![i, ..]);
+                hello_gae(
+                    m.into_slice().unwrap(),
+                    sq.into_slice().unwrap(),
+                    l.into_slice().unwrap(),
+                );
                 ns[[p, v]] += 1;
             }
         }
+
         Ok(())
     }
 
-    #[pyfn(m, "my_py")]
-    fn my_py(_py: Python, x: &mut PyArrayDyn<f64>) -> PyResult<()> {
-        let mut x = x.as_array_mut();
-        let s = x.shape();
-        for i in 0..s[0] {
-            x[i] = (i as f64) * 2.0;
-        }
-        Ok(())
-    }
     Ok(())
+}
+#[inline(never)]
+fn hello_gae(mut m: &mut [i64], mut sq: &mut [i64], l: &[i16]) {
+    m.iter_mut()
+        .zip(sq.iter_mut())
+        .zip(l.iter())
+        .for_each(|((m, sq), tr)| {
+            *m += *tr as i64;
+            *sq += (*tr as i64) * (*tr as i64);
+        });
 }
