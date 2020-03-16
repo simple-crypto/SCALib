5bfb82e2b41265a2b8a19eedaa46781fa54dab46
obronchain <olivier.bronchain@uclouvain.be>
2020-03-16 16:27:53 +0100

    simplify python SNR

--- evaluation/snr.py
+++ evaluation/snr.py
@@ -57,20 +57,21 @@ class SNR:
         elif len(X) != self._Np:
             raise Exception("Input X array does not match: Expected {} given {}".format((self._Np,len(traces)),X.shape))
         X = (X%self._Nc).astype(np.uint16)
-        I = np.arange(self._Np)
 
         if use_rust:
             rust.update_snr(traces,X,self._sum,self._sum2,self._ns,self._means,self._vars,nchunks)
         else:
             n = len(traces[:,0])
-            for i in tqdm(range(n),desc="SNR"):
-                self._ns[I,X[:,i]] += 1
-                t = traces[i,:]
-                self._sum[I,X[:,i],:] += t
-                self._sum2[I,X[:,i],:] += t*t.astype(np.int32)
-            for c in range(self._Nc):
-                self._means[I,c,:] = (self._sum[I,c,:].T / self._ns[I,c]).T
-                self._vars[I,c,:] = (self._sum2[I,c,:].T/self._ns[I,c]).T - (self._means[I,c,:]**2)
+            for v in range(self._Np):
+                for i in tqdm(range(n),desc="SNR"):
+                    self._ns[v,X[v,i]] += 1
+                    t = traces[i,:]
+                    self._sum[v,X[v,i],:] += t
+                    self._sum2[v,X[v,i],:] += t*t.astype(np.int32)
 
-        self._SNR[:] = np.var(self._means,axis=1)/np.mean(self._vars,axis=1)
+                for c in range(self._Nc):
+                    self._means[v,c,:] = (self._sum[v,c,:].T / self._ns[v,c]).T
+                    self._vars[v,c,:] = (self._sum2[v,c,:].T/self._ns[v,c]).T - (self._means[v,c,:]**2)
+
+                self._SNR[v,:] = np.var(self._means[v,:],axis=0)/np.mean(self._vars[v,:],axis=0)
         return self._SNR
