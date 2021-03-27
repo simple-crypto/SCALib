33a24690e99a1e846494993030f87c65991f6470
obronchain <olivier.bronchain@uclouvain.be>
2021-03-27 19:59:00 +0100

    comment lda

--- scale_ext/scale/src/lda.rs
+++ scale_ext/scale/src/lda.rs
@@ -35,6 +35,8 @@ pub struct LDA {
 #[pymethods]
 impl LDA {
     #[new]
+
+    /// Init an LDA with empty arrays
     fn new(_py: Python, nc: usize, p: usize, ns: usize) -> Self {
         LDA {
             cov: Array2::<f64>::zeros((p, p)),
@@ -46,23 +48,36 @@ impl LDA {
             ns: ns,
         }
     }
+    /// Fit the LDA with measurements to derive projection,means,covariance and psd.
+    /// x: traces with shape (n,ns)
+    /// y: random value realization (n,)
     fn fit(&mut self, py: Python, x: PyReadonlyArray2<i16>, y: PyReadonlyArray1<u16>) {
         let x = x.as_array();
         let y = y.as_array();
         let nc = self.nc;
         let p = self.p;
+        let ns = self.ns;
+        let n = x.shape()[0];
+
+        // release the GIL
         py.allow_threads(|| {
-            let nk = nc;
-            let ns = x.shape()[1];
-            let n = x.shape()[0];
+            // The following goes in three steps
+            // 1. Compute the projection
+            // 2. Compute the means and cov in linear subspace
+            // 3. Compute pseudo inverse of cov
 
-            let mut c_means = Array2::<i64>::zeros((nk, ns));
-            let mut s = Array2::<i64>::zeros((nk, 1));
-            let mut sb_o = Array2::<f64>::zeros((ns, ns)).reversed_axes();
-            let mut sw_o = Array2::<f64>::zeros((ns, ns)).reversed_axes();
+            // ---- Step 1
+            // compute the projection
+            // This is similar to LDA in scikit-learn with "eigen" parameter
+            // ref: https://github.com/scikit-learn/scikit-learn/blob/95119c13a/sklearn/discriminant_analysis.py#L365
 
-            // compute class means
-            c_means
+            // sum of the leakage for every possible value
+            let mut sums_ns = Array2::<i64>::zeros((nc, ns));
+            // number of realization for every value
+            let mut s = Array2::<i64>::zeros((nc, 1));
+
+            // compute class sums
+            sums_ns
                 .outer_iter_mut()
                 .into_par_iter()
                 .zip(s.outer_iter_mut().into_par_iter())
@@ -79,39 +94,53 @@ impl LDA {
                     s.fill(n);
                 });
 
-            let c_means = c_means.mapv(|x| x as f64);
+            let sums_ns = sums_ns.mapv(|x| x as f64);
             let s = s.mapv(|x| x as f64);
-            let mean_total = c_means.sum_axis(Axis(0)).insert_axis(Axis(1)) / (n as f64);
-            let c_means = &c_means / &s.broadcast(c_means.shape()).unwrap();
 
+            // mean of the traces (without carrying of the classes)
+            let mean_total = sums_ns.sum_axis(Axis(0)).insert_axis(Axis(1)) / (n as f64);
+            // mean of every class
+            let means_ns = &sums_ns / &s.broadcast(sums_ns.shape()).unwrap();
+
+            // Compute covariance matrix of x without carrying of the classes
+            // store transpose of x in f64.
+            // This consumes twice the memory but allows openblas to be used by ndarray.
             let mut x_f64 = x.mapv(|x| x as f64);
             let mut x_f64_t = Array2::<f64>::zeros(x.t().raw_dim());
             Zip::from(&mut x_f64_t)
                 .and(&x_f64.t())
                 .par_apply(|x, y| *x = *y);
-
             let st = x_f64_t.dot(&x_f64) / (n as f64) - mean_total.dot(&mean_total.t());
+
+            // Computes the covariance of the noise. That is the pooled covariance
+            // of the centered traces according to the class. x_f64 = x - means_ns[y]
+            // Using same trick with transpose for openblas
+            // reversed_axes is to use fortran layout to easily use lapack
+            let mut sw = Array2::<f64>::zeros((ns, ns)).reversed_axes();
             x_f64
                 .outer_iter_mut()
                 .into_par_iter()
                 .zip(y.outer_iter().into_par_iter())
                 .for_each(|(mut x, y)| {
                     let y = y.first().unwrap();
-                    x -= &c_means.slice(s![*y as usize, ..]);
+                    x -= &means_ns.slice(s![*y as usize, ..]);
                 });
             Zip::from(&mut x_f64_t)
                 .and(&x_f64.t())
                 .par_apply(|x, y| *x = *y);
-            sw_o.assign(&(x_f64_t.dot(&x_f64) / (n as f64)));
+            sw.assign(&(x_f64_t.dot(&x_f64) / (n as f64)));
 
-            Zip::from(&mut sb_o)
+            // sb = st - sw
+            let mut sb = Array2::<f64>::zeros((ns, ns)).reversed_axes();
+            Zip::from(&mut sb)
                 .and(&st)
-                .and(&sw_o)
+                .and(&sw)
                 .par_apply(|sb, st, sw| *sb = st - sw);
 
+            // Generalized eigenvalue decomposition for sb and sw with a call to dsysgvd
+            // Lapack routine. Eigen vectors are stored inplace in sb.
+            // See: https://www.netlib.org/lapack/explore-html/d2/d8a/group__double_s_yeigen_ga912ae48bb1650b2c7174807ffa5456ca.html
             let mut evals = vec![0.0; ns as usize];
-
-            // link to lapack sysgvd
             unsafe {
                 let mut i: i32 = 0;
                 let itype = vec![1];
@@ -124,9 +153,9 @@ impl LDA {
                     b'V',
                     b'L',
                     ns as i32,
-                    sb_o.as_allocated_mut().unwrap(),
+                    sb.as_allocated_mut().unwrap(),
                     ns as i32,
-                    sw_o.as_allocated_mut().unwrap(),
+                    sw.as_allocated_mut().unwrap(),
                     ns as i32,
                     &mut evals,
                     &mut work,
@@ -136,11 +165,13 @@ impl LDA {
                     &mut i,
                 );
             }
+
+            // Get the projection from eigen vectors and eigen values
+            let mut projection = Array2::<f64>::zeros((p, ns));
             let mut index: Vec<(usize, f64)> = (0..evals.len()).zip(evals).collect();
-            let evecs = sb_o;
+            let evecs = sb;
             index.sort_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap());
             index.reverse();
-            let mut projection = Array2::<f64>::zeros((p, index.len()));
             index
                 .iter()
                 .zip(projection.axis_iter_mut(Axis(0)))
@@ -148,35 +179,48 @@ impl LDA {
                     proj.assign(&evecs.slice(s![.., *i]));
                 });
 
-            // compute mean and cov
-            let means = projection.dot(&c_means.t());
-            let traces_t = projection.dot(&x_f64_t); // (n,p)
+            // ---- Step 2
+            // means per class within the subspace by projecting means_ns
+            let means = projection.dot(&means_ns.t());
+            // project the centered traces to subspace
+            let traces_t = projection.dot(&x_f64_t); // shape (p,n)
 
+            // compute the noise covariance in the linear subspace
             let mut traces_t_t = Array2::zeros(traces_t.t().raw_dim());
             Zip::from(&mut traces_t_t)
                 .and(&traces_t.t())
                 .par_apply(|x, y| *x = *y);
+            let cov = traces_t.dot(&traces_t_t) / (n as f64);
 
-            let cov = traces_t.dot(&traces_t_t) / (traces_t.shape()[1] as f64);
+            // ---- Step 3
+            // Compute pseudo inverse of covariance matrix
+            // This is a translation of _PSD in scipy.multivariate
+            // ref: https://github.com/scipy/scipy/blob/5ab7426247900db9de856e790b8bea1bd71aec49/scipy/stats/_multivariate.py#L115
 
             let (s, u) = cov.eigh(UPLO::Lower).unwrap();
+            // threshold for eigen values
             let cond =
                 (cov.len() as f64) * (s.fold(f64::MIN, |acc, x| acc.max(f64::abs(*x)))) * 2.3E-16;
 
+            // Only eigen values larger than cond and their id
+            // vec<(id,eigenval)>
             let pack: Vec<(usize, &f64)> = (0..s.len())
                 .zip(s.into_iter())
                 .filter(|(_, &s)| f64::abs(s) > cond)
                 .collect();
 
+            // corresponding eigen vectors
             let mut u_s = Array2::<f64>::zeros((u.shape()[0], pack.len()));
             pack.iter()
                 .zip(u_s.axis_iter_mut(Axis(1)))
                 .for_each(|((i, _), mut u_s)| u_s.assign(&u.slice(s![.., *i])));
 
+            //  psd = eigen_vec * (1/sqrt(eigen_val))
             let psigma_diag: Array1<f64> =
                 pack.into_iter().map(|(_, x)| 1.0 / f64::sqrt(*x)).collect();
             let psd = &u_s * &psigma_diag.broadcast(u_s.shape()).unwrap();
 
+            // store the results
             self.cov.assign(&cov);
             self.psd.assign(&psd);
             self.means.assign(&means);
@@ -184,6 +228,71 @@ impl LDA {
         })
     }
 
+    /// return the probability of each of the possible value for leakage samples
+    /// x : traces with shape (n,ns)
+    /// return prs with shape (n,nc). Every row corresponds to one probability distribution
+    fn predict_proba<'py>(
+        &mut self,
+        py: Python<'py>,
+        x: PyReadonlyArray2<i16>,
+    ) -> PyResult<&'py PyArray2<f64>> {
+        // This function is based on scipy.mutlivariate.pdf()
+        // ref: https://github.com/scipy/scipy/blob/5ab7426247900db9de856e790b8bea1bd71aec49/scipy/stats/_multivariate.py#L452
+
+        let x = x.as_array();
+        let n = x.shape()[0];
+        let means = &self.means.t();
+        let psd = &self.psd;
+        let projection = &self.projection;
+        let ns = self.ns;
+        let p = self.p;
+
+        let mut prs = Array2::<f64>::zeros((n, means.shape()[0]));
+        py.allow_threads(|| {
+            x.outer_iter()
+                .into_par_iter()
+                .zip(prs.outer_iter_mut().into_par_iter())
+                .for_each_init(
+                    || {
+                        (
+                            Array1::<f64>::zeros((ns,)),
+                            Array1::<f64>::zeros((p,)),
+                            Array1::<f64>::zeros((p,)),
+                        )
+                    },
+                    |(ref mut x_i, ref mut x_proj, ref mut mu), (x, mut prs)| {
+                        x_i.zip_mut_with(&x, |x, y| *x = *y as f64);
+                        // project trace for subspace
+                        x_proj.assign(
+                            &projection
+                                .axis_iter(Axis(1))
+                                .map(|p| {
+                                    p.iter()
+                                        .zip(x_i.iter())
+                                        .fold(0.0, |acc, (p, x_i)| acc + p * x_i)
+                                })
+                                .collect::<Array1<f64>>(),
+                        );
+
+                        prs.assign(
+                            &(means
+                                .outer_iter()
+                                .map(|means| {
+                                    mu.assign(&means);
+                                    mu.zip_mut_with(&x_proj, |mu, x_proj| *mu -= x_proj);
+                                    psd.dot(mu).fold(0.0, |acc, x| acc + x.powi(2))
+                                })
+                                .collect::<Array1<f64>>()),
+                        );
+                        prs.mapv_inplace(|x| f64::exp(-0.5 * x));
+                        prs /= prs.sum();
+                    },
+                );
+        });
+        Ok(&(prs.to_pyarray(py)))
+    }
+
+    /// Set the LDA state based on all its parameters
     fn set_state<'py>(
         &mut self,
         _py: Python<'py>,
@@ -204,57 +313,7 @@ impl LDA {
         self.ns = ns;
     }
 
-    fn predict_proba<'py>(
-        &mut self,
-        py: Python<'py>,
-        x: PyReadonlyArray2<i16>,
-    ) -> PyResult<&'py PyArray2<f64>> {
-        let x = x.as_array();
-        let c_means = &self.means.t();
-        let psd = &self.psd;
-        let projection = &self.projection;
-
-        let ns_in = self.ns;
-        let ns_proj = projection.shape()[1];
-        let mut prs = Array2::<f64>::zeros((x.shape()[0], c_means.shape()[0]));
-        py.allow_threads(|| {
-            x.axis_chunks_iter(Axis(0), 100)
-                .into_par_iter()
-                .zip(prs.axis_chunks_iter_mut(Axis(0), 100).into_par_iter())
-                .for_each(|(x, mut prs)| {
-                    let mut x_i = Array1::<f64>::zeros(ns_in);
-                    let mut x_proj = Array1::<f64>::zeros(ns_proj);
-                    let mut mu = Array1::<f64>::zeros(ns_proj);
-
-                    x.outer_iter()
-                        .zip(prs.outer_iter_mut())
-                        .for_each(|(x, mut prs)| {
-                            x_i = x.mapv(|x| x as f64);
-                            // project trace for subspace
-                            x_proj.assign(
-                                &projection
-                                    .axis_iter(Axis(1))
-                                    .map(|p| (&x_i * &p).sum())
-                                    .collect::<Array1<f64>>(),
-                            );
-
-                            prs.assign(
-                                &c_means
-                                    .outer_iter()
-                                    .map(|c_means| {
-                                        mu = &c_means - &x_proj;
-                                        psd.dot(&mut mu).fold(0.0, |acc, x| acc + x.powi(2))
-                                    })
-                                    .collect::<Array1<f64>>(),
-                            );
-                            prs.mapv_inplace(|x| f64::exp(-0.5 * x));
-                            prs /= prs.sum();
-                        });
-                });
-        });
-        Ok(&(prs.to_pyarray(py)))
-    }
-
+    /// Get LDA internal data
     fn get_cov<'py>(&self, py: Python<'py>) -> PyResult<&'py PyArray2<f64>> {
         Ok(&self.cov.to_pyarray(py))
     }
