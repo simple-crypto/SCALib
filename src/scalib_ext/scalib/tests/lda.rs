eff34ee26e85ada6b7b97e5dd7e7acde10867347
obronchain <olivier.bronchain@uclouvain.be>
2021-04-23 14:11:17 +0200

    add rust tests for LDA

---
+++ src/scalib_ext/scalib/tests/lda.rs
@@ -0,0 +1,35 @@
+use ndarray::{Array1, Array2};
+use ndarray_rand::rand_distr::Uniform;
+use ndarray_rand::RandomExt;
+use scalib::lda;
+
+extern crate openblas_src as _;
+
+#[test]
+fn fit_lda() {
+    let ns = 100;
+    let n = 1000;
+    let nc = 16;
+    let p = 4;
+
+    let mut lda = lda::LDA::new(nc, p, ns);
+    let x = Array2::<i16>::random((n, ns), Uniform::new(0, 100));
+    let y = Array1::<u16>::random(n, Uniform::new(0, nc as u16));
+
+    lda.fit(x.view(), y.view());
+}
+
+#[test]
+fn predict_lda() {
+    let ns = 100;
+    let n = 1000;
+    let nc = 16;
+    let p = 4;
+
+    let mut lda = lda::LDA::new(nc, p, ns);
+    let x = Array2::<i16>::random((n, ns), Uniform::new(0, 100));
+    let y = Array1::<u16>::random(n, Uniform::new(0, nc as u16));
+
+    lda.fit(x.view(), y.view());
+    lda.predict_proba(x.view());
+}
