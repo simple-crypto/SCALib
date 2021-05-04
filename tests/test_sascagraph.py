ce3f178a9198703c46169dd8a99775275943f58c
obronchain <olivier.bronchain@uclouvain.be>
2021-05-04 10:18:38 +0200

    better tests for Xor

--- tests/test_sascagraph.py
+++ tests/test_sascagraph.py
@@ -105,31 +105,51 @@ def test_xor():
     """
     Test XOR between distributions
     """
-    nc = 16
-    n = 100
-    distri_x = np.random.randint(1, 100, (n, nc))
+    nc = 512
+    n = 1
+    distri_x = np.random.randint(1, 10000000, (n, nc))
     distri_x = (distri_x.T / np.sum(distri_x, axis=1)).T
-    distri_y = np.random.randint(1, 100, (n, nc))
+    distri_y = np.random.randint(1, 10000000, (n, nc))
     distri_y = (distri_y.T / np.sum(distri_y, axis=1)).T
+
+    distri_b = np.random.randint(1, 10000000, (n, nc))
+    distri_b = (distri_b.T / np.sum(distri_b, axis=1)).T
+    distri_a = np.random.randint(1, 10000000, (n, nc))
+    distri_a = (distri_a.T / np.sum(distri_a, axis=1)).T
+
 
     graph = f"""
         # some comments
         NC {nc}
         PROPERTY z = x^y
+        PROPERTY x = a^b
         VAR MULTI x # some comments too
         VAR MULTI y
-        VAR MULTI z"""
+        VAR MULTI a
+        VAR MULTI b
+        VAR SINGLE z"""
 
     graph = SASCAGraph(graph, n)
     graph.set_init_distribution("x", distri_x)
     graph.set_init_distribution("y", distri_y)
+    graph.set_init_distribution("a", distri_a)
+    graph.set_init_distribution("b", distri_b)
 
-    graph.run_bp(1)
+
+    graph.run_bp(10)
     distri_z = graph.get_distribution("z")
 
     distri_z_ref = np.zeros(distri_z.shape)
+    msg = np.zeros(distri_z.shape)
+ 
+    for a in range(nc):
+        for b in range(nc):
+            msg[:, a ^ b] += distri_a[:, a] * distri_b[:, b]
+    
+    distri_x *= msg
     for x in range(nc):
         for y in range(nc):
             distri_z_ref[:, x ^ y] += distri_x[:, x] * distri_y[:, y]
 
+    distri_z_ref = (distri_z_ref.T / np.sum(distri_z_ref, axis=1)).T
     assert np.allclose(distri_z_ref, distri_z)
