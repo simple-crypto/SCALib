34390f385ecd02a9f0e4418538afae116eb20f7b
obronchain <olivier.bronchain@uclouvain.be>
2021-06-28 14:01:07 +0200

    update the benches

--- src/scalib_ext/scalib/benches/snr_update.rs
+++ src/scalib_ext/scalib/benches/snr_update.rs
@@ -6,14 +6,14 @@ use scalib::snr;
 
 fn bench_snr_update(c: &mut Criterion) {
     let mut group = c.benchmark_group("snr_update");
-    let nc = 256;
-    let np = 32;
-    let n = 10;
+    let nc = 16;
+    let np = 16;
+    let n = nc*500;
     for ns in (10..20).map(|x| 2.0_f64.powi(x) as usize) {
         let x = Array2::<i16>::random((n, ns), Uniform::new(0, 10000));
         let y = Array2::<u16>::random((n, np), Uniform::new(0, nc as u16));
 
-        let mut snr = snr::SNR::new(nc, ns, np);
+                let mut snr = snr::SNR::new(nc, ns, np);
         snr.update(x.view(), y.view());
 
         group.bench_with_input(BenchmarkId::new("snr_update", ns), &ns, |b, ns| {
@@ -28,7 +28,7 @@ fn bench_snr_update(c: &mut Criterion) {
 criterion_group! {
     name = benches;
     // This can be any expression that returns a `Criterion` object.
-    config = Criterion::default().significance_level(0.01).sample_size(20);
+    config = Criterion::default();
     targets = bench_snr_update
 }
 criterion_main!(benches);
