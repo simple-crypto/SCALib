9b7afa1b22d9c407b4b1765bc2047550737857b4
Olivier Bronchain <olivier.bronchain@gmail.com>
2022-05-26 10:07:03 +0200

    updating the tests for SNR

--- tests/test_snr.py
+++ tests/test_snr.py
@@ -4,7 +4,8 @@ import numpy as np
 
 
 def gen_snr_data(nv, nc, ns, n):
-    x = np.random.randint(0, 256, (n, ns), dtype=np.int16) - 128
+    ii16 = np.iinfo(np.int16)
+    x = np.random.randint(ii16.min + 1, ii16.max, (n, ns), dtype=np.int16)
     y = np.random.randint(0, nc, (n, nv), dtype=np.uint16)
     return x, y
 
@@ -26,20 +27,27 @@ def snr_pooled(x, y, nv, nc, ns):
     return snr_ref
 
 
-def snr_pooled2(x, y, nv, nc, ns):
+def snr_pooled2(x, y, nv, nc, ns, use_64bit):
     """Should be almost exact implementation of rust version, including rounding
     errors."""
+
+    if use_64bit:
+        stype = np.int64
+    else:
+        stype = np.int32
+
     sig = np.zeros((nv, ns), dtype=np.float64)
     no = np.zeros((nv, ns), dtype=np.float64)
-    g_sum = np.sum(x, axis=0).astype(object)
-    sum_sq = np.sum(x**2, axis=0).astype(object)
+    g_sum = np.sum(x.astype(np.int64), axis=0).astype(object)
+    sum_sq = np.sum(x.astype(np.int64) ** 2, axis=0).astype(object)
+
     n = x.shape[0]
     for v in range(nv):
         sum_sq_cls = np.zeros((ns,)).astype(object)
         for c in range(nc):
             samples = x[np.where(y.transpose()[v] == c)[0], :]
             if samples.shape[0] != 0:
-                s = np.sum(samples, axis=0)
+                s = np.sum(samples.astype(stype), axis=0).astype(object)
                 sum_sq_cls += (s**2).astype(object) * n // samples.shape[0]
         sig[v, :] = (sum_sq_cls - g_sum**2).astype(np.float64)
         assert n == x.shape[0]
@@ -57,18 +65,21 @@ def snr_run(nc, nv, ns, n, use_64bit, batch=100):
 
     # compute SNR with SCALib
     snr = SNR(np=nv, nc=nc, ns=ns, use_64bit=use_64bit)
+
+    n_samples = np.array([[(y[:, v] == c).sum() for c in range(nc)] for v in range(nv)])
+
+    if (n_samples > (2**16)).any() and not use_64bit:
+        with pytest.raises(Exception):
+            for i in range(0, n, batch):
+                snr.fit_u(x[i : i + batch, :], y[i : i + batch, :])
+            snr_val = snr.get_snr()
+        return
+
     for i in range(0, n, batch):
         snr.fit_u(x[i : i + batch, :], y[i : i + batch, :])
     snr_val = snr.get_snr()
 
-    sums = np.array(
-        [[x[y[:, v] == c, :].sum(axis=0) for c in range(nc)] for v in range(nv)],
-        dtype=np.int32,
-    )
-    sum_square = (x**2).sum(axis=0)
-    n_samples = np.array([[(y[:, v] == c).sum() for c in range(nc)] for v in range(nv)])
-
-    snr_ref = snr_pooled2(x, y, nv, nc, ns)
+    snr_ref = snr_pooled2(x, y, nv, nc, ns, use_64bit)
     assert np.allclose(snr_ref, snr_val, equal_nan=True)
     snr_ref = snr_pooled(x, y, nv, nc, ns)
     if not (n_samples == 0).any():
@@ -87,6 +98,7 @@ def test_snr():
         {"nc": 16, "nv": 153, "ns": 10, "n": 10},
         {"nc": 1024, "nv": 3, "ns": 10, "n": 10},
         {"nc": 2, "nv": 3, "ns": 10**4, "n": 10},
+        {"nc": 2, "nv": 1, "ns": 1, "n": 1 << 17},
     ]
     for test_case in test_cases:
         for use_64bit in [False, True]:
