91f5c828e9c2cb0a23c33f1d93589d76de13e73b
Rishub <6591138+rishubn@users.noreply.github.com>
2024-02-27 15:46:02 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -60,9 +60,8 @@ autodoc_mock_imports = [
     "cpuinfo",
 ]
 
-user_agent = (
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0"
-)
+user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0"
+
 
 
 import sys
