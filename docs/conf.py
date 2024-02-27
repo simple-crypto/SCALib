8e218908dc78e0950d78abde4d219ed2c8a78652
Rishub <6591138+rishubn@users.noreply.github.com>
2024-02-27 15:50:13 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -60,8 +60,10 @@ autodoc_mock_imports = [
     "cpuinfo",
 ]
 
-user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0"
-
+linkcheck_request_headers = {
+    r'https://mathoverflow.net/': {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; '
+                                                'rv:24.0) Gecko/20100101 Firefox/24.0'}
+}
 
 
 import sys
