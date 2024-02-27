9602a4e7a3e7fbe0d4e194775c0edb1eb3ffd391
Rishub <6591138+rishubn@users.noreply.github.com>
2024-02-27 16:27:18 +0100

    Fix docs failing (#143)
    * add user agent string
    
    * Update conf.py
    
    * Update conf.py
    
    * Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,68 +2,70 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'SCALib'
-copyright = 'SIMPLE Contributors'
+project = "SCALib"
+copyright = "SIMPLE Contributors"
 
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.coverage',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinxcontrib.bibtex',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.coverage",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinxcontrib.bibtex",
 ]
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-bibtex_bibfiles = ['refs.bib']
+bibtex_bibfiles = ["refs.bib"]
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'furo'
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 
-autoclass_content = 'both'
+autoclass_content = "both"
 
 autosummary_ignore_module_all = False
 
 autodoc_default_options = {
-    'member-order': 'bysource',
-    'undoc-members': True,
-    'special-members': False,
+    "member-order": "bysource",
+    "undoc-members": True,
+    "special-members": False,
 }
 
-autodoc_typehints = 'description'
+autodoc_typehints = "description"
 
 # We do not need to install an build the package to generate the doc.
 # We only have to mock dependencies, binary packages, and add the source to the
 # python path.
 autodoc_mock_imports = [
-        "scalib._scalib_ext",
-        "scalib.version",
-        "scalib.build_config",
-        "numpy",
-        "cpuinfo",
-        ]
+    "scalib._scalib_ext",
+    "scalib.version",
+    "scalib.build_config",
+    "numpy",
+    "cpuinfo",
+]
+
+linkcheck_ignore = [r'https://mathoverflow.net/a/273060']
 
 import sys
-sys.path.append('../src')
 
-#from scalib.version import version
+sys.path.append("../src")
+
+# from scalib.version import version
 # The full version, including alpha/beta/rc tags
-#release = version
-
+# release = version
