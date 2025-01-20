e66a27acdd198b00e8fb698101bc7ad7e80c92ea
Gaëtan Cassiers <gaetan.cassiers@uclouvain.be>
2025-01-20 21:19:47 +0100

    Remove py-cpuinfo dependency, roll our own implementation.
    cpuinfo was very slow to import, making "import scalib" take a
    noticeable amount of time, which was annoying.
    We replace it with a new native library implemented in rust, that uses
    rust stdlib feature detection.

--- src/scalib/__init__.py
+++ src/scalib/__init__.py
@@ -9,38 +9,21 @@ __all__ = [
     "ScalibError",
 ]
 
-import cpuinfo
 
 from .build_config import REQUIRE_X86_64_V3
-from .version import version as __version__
+from .version import version as __version__  # noqa: F401
 
-cpu = cpuinfo.get_cpu_info()
+from ._cpu_check import support_x86_64_v3
 
-X86_64_V3_FEATURES = [
-    "AVX",
-    "AVX2",
-    "BMI1",
-    "BMI2",
-    # Some features not tested, shouldn't be an issue.
-    # "f16c",
-    # "fma",
-    # "lzcnt",
-    # "movbe",
-    # "osxsave",
-]
-
-if (
-    REQUIRE_X86_64_V3
-    and cpu["arch"] == "X86_64"
-    and any(f.lower() not in cpu["flags"] for f in X86_64_V3_FEATURES)
-):
-    mf = ", ".join(f for f in X86_64_V3_FEATURES if f not in cpu["flags"])
+if REQUIRE_X86_64_V3 and not all(s for _, s in support_x86_64_v3()):
+    # mf = ", ".join(f for f in X86_64_V3_FEATURES if f not in cpu["flags"])
     raise ImportError(
         "This SCALib build requires x86-64-v3 level processor features, which "
         + "are not all supported by this CPU or OS."
         + "See https://github.com/simple-crypto/SCALib/blob/main/README.rst "
         + "for compiling a SCALib adapted to your CPU."
-        + f"(Missing features: {mf})."
+        + f"Missing features: {[f for f, a in support_x86_64_v3() if not a]}."
+        #    + f"(Missing features: {mf})."
     )
 
 from scalib._scalib_ext import ScalibError
