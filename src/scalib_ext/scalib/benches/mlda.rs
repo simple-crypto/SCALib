e755afbeed5411ae452eeec9f5c4054e38734ec1
cmomin <charles.momin@uclouvain.be>
2025-03-13 10:40:44 +0100

    add benches for univariate comparison

--- src/scalib_ext/scalib/benches/mlda.rs
+++ src/scalib_ext/scalib/benches/mlda.rs
@@ -1,10 +1,11 @@
 //use std::fmt::format;
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use ndarray::Array2;
+use ndarray::{Array1, Array2};
 use ndarray_rand::RandomExt;
 use ndarray_rand::{rand::SeedableRng, rand_distr::Uniform};
-use scalib::multi_lda::{Class, MultiLdaAcc};
+use scalib::lda::{LdaAcc, LDA};
+use scalib::multi_lda::{Class, MultiLda, MultiLdaAcc};
 
 use ndarray_rand::rand::prelude::SliceRandom;
 use rand_xoshiro::Xoshiro256StarStar as Prng;
@@ -55,6 +56,13 @@ fn bench_mlda_ll_update(mlda: &mut MultiLdaAcc, traces: &Array2<i16>, classes: &
     let _ = mlda.update(traces.view(), classes.view());
 }
 
+fn generate_case_data(nv: usize, nc: Class, ns: u32, n: usize) -> (Array2<i16>, Array2<u16>) {
+    // Genereate the useful data
+    let t = gen_traces(n, ns as usize);
+    let x = gen_classes(nv, n, nc as usize);
+    (t, x)
+}
+
 fn generate_inputs_mlda_ll_update(
     seed: u64,
     nc: Class,
@@ -68,8 +76,7 @@ fn generate_inputs_mlda_ll_update(
     let mut prng = Prng::seed_from_u64(seed);
     let pois: Vec<Vec<u32>> = gen_pois_with_maxpois(npois, ns, max_npois, nv, &mut prng);
     // Genereate the useful data
-    let t = gen_traces(n, ns as usize);
-    let x = gen_classes(nv, n, nc as usize);
+    let (t, x) = generate_case_data(nv, nc, ns, n);
     // Instanciate the mlda object
     let mlda = MultiLdaAcc::new(ns, nc, pois).unwrap();
     // Return
@@ -191,12 +198,127 @@ fn bench_mlda_update_top(c: &mut Criterion) {
     bench_mlda_ll_update_all(seed, nc, 10000, 1, 1000, 10000, n, &mut group);
     bench_mlda_ll_update_all(seed, nc, 10000, 100, 1, 10000, n, &mut group);
     bench_mlda_ll_update_all(seed, nc, 10000, 100, 1000, 10000, n, &mut group);
+    bench_mlda_ll_update_all(seed, nc, 10000, 1000, 1, 10000, n, &mut group);
+    bench_mlda_ll_update_all(seed, nc, 10000, 1000, 1000, 10000, n, &mut group);
+}
+
+/////// Next, similar but with the old Lda object to check the performances
+// Update only
+fn bench_univariate_old_lda_update(nc: Class, ns: u32, n: usize, group: &mut BenchMarkGroup) {
+    // Sums
+    group.bench_with_input(
+        BenchmarkId::new(format!("UNI-UPDATE-OLD nv:{} ; ns:{}", 1, ns), ns),
+        &ns,
+        |b, _| {
+            // Generate the inputs
+            let (t, _) = generate_case_data(1, nc, ns, n);
+            let x = Array1::<u16>::random(n, Uniform::new(0, nc as u16));
+            let mut lda = LdaAcc::new(nc as usize, t.view(), x.view(), 1);
+            b.iter(|| {
+                // Create the old mlda object will do an update on the fly
+                lda.update(t.view(), x.view(), 1);
+            })
+        },
+    );
+}
+
+fn bench_univariate_mlda_update(nc: Class, ns: u32, n: usize, group: &mut BenchMarkGroup) {
+    // Sums
+    group.bench_with_input(
+        BenchmarkId::new(format!("UNI-UPDATE-NEW nv:{} ; ns:{}", 1, ns), ns),
+        &ns,
+        |b, _| {
+            // Pois, which is in fact all the time samples
+            let pois: Vec<Vec<u32>> = vec![(0..ns).into_iter().collect()];
+            // Creation of the Mlda instance
+            let mut mlda = MultiLdaAcc::new(ns, nc, pois).unwrap();
+            // Generate the inputs
+            let (t, x) = generate_case_data(1, nc, ns, n);
+            b.iter(|| {
+                let _ = mlda.update(t.view(), x.view());
+            })
+        },
+    );
+}
+
+fn bench_univariate_update(nc: Class, ns: u32, n: usize, group: &mut BenchMarkGroup) {
+    bench_univariate_old_lda_update(nc, ns, n, group);
+    bench_univariate_mlda_update(nc, ns, n, group);
+}
+
+// "Solve" operation
+fn bench_univariate_old_lda_solve(
+    nc: Class,
+    ns: u32,
+    n: usize,
+    p: u32,
+    group: &mut BenchMarkGroup,
+) {
+    // Sums
+    group.bench_with_input(
+        BenchmarkId::new(
+            format!("UNI-SOLVE-OLD nv:{} ; ns:{} ; ndim:{}", 1, ns, p),
+            p,
+        ),
+        &p,
+        |b, _| {
+            // Generate the inputs
+            let (t, _) = generate_case_data(1, nc, ns, n);
+            let x = Array1::<u16>::random(n, Uniform::new(0, nc as u16));
+            // Create the old mlda object will do an update on the fly
+            let mut lda = LdaAcc::new(nc as usize, t.view(), x.view(), 1);
+            lda.update(t.view(), x.view(), 1);
+            b.iter(|| {
+                let _ldares = lda.lda(p as usize).unwrap();
+            })
+        },
+    );
+}
+
+fn bench_univariate_mlda_solve(nc: Class, ns: u32, n: usize, p: u32, group: &mut BenchMarkGroup) {
+    // Sums
+    group.bench_with_input(
+        BenchmarkId::new(
+            format!("UNI-SOLVE-NEW nv:{} ; ns:{} ; ndim:{}", 1, ns, p),
+            p,
+        ),
+        &p,
+        |b, _| {
+            // Pois, which is in fact all the time samples
+            let pois: Vec<Vec<u32>> = vec![(0..ns).into_iter().collect()];
+            // Creation of the Mlda instance
+            let mut mlda = MultiLdaAcc::new(ns, nc, pois).unwrap();
+            // Generate the inputs
+            let (t, x) = generate_case_data(1, nc, ns, n);
+            let _ = mlda.update(t.view(), x.view());
+            let config = scalib::Config::no_progress();
+            b.iter(|| {
+                let _mldares = mlda.lda(p, &config);
+            })
+        },
+    );
+}
+
+fn bench_univariate_solve(nc: Class, ns: u32, n: usize, p: u32, group: &mut BenchMarkGroup) {
+    bench_univariate_old_lda_solve(nc, ns, n, p, group);
+    bench_univariate_mlda_solve(nc, ns, n, p, group);
+    println!("\n");
+}
+
+fn bench_univariate(c: &mut Criterion) {
+    let mut group = c.benchmark_group("LDA vs MLDA");
+    let ns = 1000;
+    let nc = 256;
+    let n = 10000;
+    bench_univariate_update(nc, ns, n, &mut group);
+    bench_univariate_solve(nc, ns, n, 1, &mut group);
+    bench_univariate_solve(nc, ns, n, 8, &mut group);
 }
 
 criterion_group! {
     name = benches;
     // This can be any expression that returns a `Criterion` object.
     config = Criterion::default().significance_level(0.1).sample_size(10);
-    targets = bench_mlda_update_top //h_mlda_init, bench_mlda_update_sums, bench_mlda_update_covs
+    targets = bench_mlda_update_top, bench_univariate
 }
 criterion_main!(benches);
