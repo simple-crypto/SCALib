800520d4e8b19f9aacfcf880904c1c99a49954c8
beguinotj <julien.beguinot@ip-paris.fr>
2025-10-20 12:13:51 +0200

    update based on ciode review

--- src/scalib/postprocessing/leakage_to_success.py
+++ src/scalib/postprocessing/leakage_to_success.py
@@ -2,17 +2,39 @@ r"""Lower bounds on the log-guessing entropy, log of the guessing entropy, log o
 
 The bounds depends on the leakage as measured by mutual information, the size of the secret key (number of bits) and eventually the number of key enumerated.
 
-Examples
---------
->>> from scalib.postprocessing import success_rate, guessing_entropy, log_guessing_entropy, median
->>> import numpy as np
->>> key_size=128
->>> enumeration_effort=32
->>> mi = np.random.uniform(low=0, high=key_size, size=10**3)
->>> sr = success_rate(mi,key_size,enumeration_effort)
->>> med = median(mi,key_size)
->>> lg = log_guessing_entropy(mi,key_size)
->>> ge = guessing_entropy(mi, key_size)
+Let say that an adversary acquires side channel information about a uniformly distributed secret K  through some side channel information Y.
+At the end of a side channel attack the adversary produces a ranking of the different key hypotheses from most likely to least likely. 
+We let :math:`\mathrm{rank}(K|Y)` be the position of the correct key in this ranking. 
+The probability distribution of :math:`\mathrm{rank}(K|Y)` provides securitry guarantees about the performances of the attacker.  
+It is hard to obtain :math:`\mathrm{rank}(K|Y)` for a full key, hoverwer, it is often possible to upper bound the informational leakages :math:`I(K;Y)`.
+Using :math:`I(K;Y)` we provide bounds on some statistics of the rank:
+
+* The probability of success of level L : 
+
+.. math::
+    \mathbb{P}_{s,L}(K|Y) = \mathbb{P}( \mathrm{rank}(K|Y) \leq L), 
+
+which bounds the probability of a successful attack when L keys are enumerated; 
+
+* The guessing entropy : 
+
+.. math::
+    G(K|Y) = \mathbb{E}[\mathrm{rank}(K|Y)], 
+
+which corresponds to the average enumeration time to recover the secret key; 
+
+* The median rank, which is the minimum number of hypotheses to enumerate to achieve a success rate of 1/2 i.e. med is the mimimum value such that 
+
+.. math::
+    \mathrm{Med}(K|Y) = \min \{ m \mid \mathbb{P}( \mathrm{rank}(K|Y) \leq m) \geq 1/2 \}; 
+
+* The log-guessing entropy,
+
+.. math:: 
+    \mathrm{LG}(K|Y) = \mathbb{E}[ \log_2 \mathrm{rank}(K|Y)],
+
+which is corresponds to the average security parameter of the implementation (it is more conservative than the guessing entropy).
+
 
 Reference
 ---------
@@ -39,14 +61,27 @@ References
 __all__ = ["success_rate", "guessing_entropy", "log_guessing_entropy", "median"]
 import numpy as np
 from scipy.special import gamma, bernoulli, factorial
+from scipy.optimize import brentq
 
 
-def binary_divergence_log(
-    log_p: np.ndarray[float], log_q: np.ndarray[float]
-) -> np.ndarray[float]:
+def binary_divergence_log(log_p: float, log_q: float) -> float:
+    r"""Outputs the binary divergence d(p||q) in a numerically stable way when log(p) and log(q) are given.
+
+    Parameters
+    ----------
+    log_p, log_q : float
+
+    Returns
+    -------
+    An estimation of the binary divergence d(p||q).
+    """
+    if log_p == 0:
+        return -log_q
+
     p = np.exp(log_p)
     q = np.exp(log_q)
-    return p * (log_p - log_q) + (1 - p) * (np.log1p(-p) - np.log1p(-q))
+
+    return p * (log_p - log_q) - np.expm1(log_p) * (np.log1p(-p) - np.log1p(-q))
 
 
 def success_rate(
@@ -54,11 +89,13 @@ def success_rate(
     key_size: int,
     enumeration_effort: float,
     base=2,
-    niter=30,
-):
-    r"""Output an upper bound on the logarithm in base 'base' of the probability of correctly guessing a secret key of of size key_size bits
+) -> np.ndarray[float]:
+    r"""Output an upper bound on the logarithm in base 2 of the probability of correctly guessing a secret key of of size key_size bits
     when a leakage upper bounded by the mutual information 'mutual_information' is disclosed to the adversary and the adversary is allowed to enumerate up to
-    2 ** enumeration_effort key hypotheses.
+    :math:`L = 2^{\mathrm{enumeration \; effort}}` key hypotheses.
+
+    .. math::
+        \log_2 \mathbb{P}_{s,L}(K|Y) = \log_2 \mathbb{P}( \mathrm{rank}(K|Y) \leq L).
 
     Parameters
     ----------
@@ -77,54 +114,91 @@ def success_rate(
     Returns
     -------
     Output an upper bound on the logarithm in base 'base' of the probability of correctly guessing a secret key of of size key_size bits when a leakage upper bounded by the mutual information 'mutual_information' is disclosed to the adversary and the adversary is allowed to enumerate up to 2^enumeration_effort key_hypotheses.
-    """
 
-    MI_nats = mutual_information * np.log(base)
+
+    Examples
+    --------
+    >>> from scalib.postprocessing import success_rate
+    >>> import numpy as np
+    >>> key_size=128
+    >>> enumeration_effort=32
+    >>> mi = np.random.uniform(low=0, high=key_size, size=1)
+    >>> sr = success_rate(mi,key_size,enumeration_effort)
+    """
+    if not (mutual_information >= 0).all():
+        raise ValueError("Invalid inputs, the mutual information should be positive.")
+    if not (mutual_information * np.log2(base) <= key_size).all():
+        raise ValueError(
+            "Invalid inputs, the mutual information in bits should be less than the key_size."
+        )
+
+    mutual_information = np.atleast_1d(mutual_information)
+    mi_nats = mutual_information * np.log(base)
     log_q = (enumeration_effort - key_size) * np.log(2)  # in nats
 
-    # Initial intervall for the dichotomic search
-    log_p_lb = np.array(log_q).reshape((1,))
-    log_p_ub = np.array(0).reshape((1,))
+    log_sr = np.empty_like(mutual_information)
+    for i in range(len(mutual_information)):
+        if mi_nats[i] >= -log_q:
+            log_sr[i] = 0
+        else:
+            log_sr[i] = brentq(
+                lambda log_p: binary_divergence_log(log_p, log_q) - mi_nats[i],
+                0,
+                log_q,
+                xtol=10**-2,
+                maxiter=100,
+                full_output=False,
+            )
 
-    # Dichotomic serach
-    for _ in range(niter):
-        log_p_mid = (log_p_lb + log_p_ub) / 2
-        y = binary_divergence_log(log_p_mid, log_q) - MI_nats
-        log_p_lb, log_p_ub = np.where(
-            y > 0, (log_p_lb, log_p_mid), (log_p_mid, log_p_ub)
-        )
-    return log_p_ub / np.log(base)  # in base 'base'
-
-
-def f(x):
-    fx = np.zeros_like(x)
-    mask = x > 0
-    fx[mask] = x[mask] - np.expm1(x[mask]) * np.log1p(-np.exp(-x[mask]))
-    return fx
+    return log_sr / np.log(2)  # in bits
 
 
-def f_inv(y, niter=20):
+def massey_inequality(x: float) -> float:
+    r"""
+    This sub-function implements the optimal version of Massey's inequality.
 
+    Massey's inequality when optimaly writen reads as  H(X|Y) >= G(X|Y) Hb( G(X|Y)^{-1}) where Hb is the binary entropy.
+
+    Let x = log( G(X|Y) ) this reads as  H(X|Y) >= x - (exp(x)-1) * log(1 - exp(-x)).
+
+    We implement this function using exp1m and log1p to be stable.
+    """
+    if x == 0:
+        return 0
+    return x - np.expm1(x) * np.log1p(-np.exp(-x))
+
+
+def massey_inequality_inverse(y: float) -> float:
+    r"""
+    This sub-function implements the inverse of the optimal Massey's inequality using brent's rootfinding algorithm.
+    """
     # Ensure that y is an array
     y = np.atleast_1d(y)
 
-    # Initial guess
-    x_lb = np.maximum(np.log(1), y - 1 + np.log1p(np.exp(1 - y) / 2)).reshape((-1,))
-    x_ub = np.maximum(np.log(2), y - 1 + np.log1p(np.exp(1 - y) / 2 + 1)).reshape((-1,))
+    inverse = np.zeros_like(y)
+    for i in range(len(y)):
+        # A precise approximation of the inverse would be y  - 1 + log(1 + exp(1-y[i])/2)
+        lb = np.maximum(0, y[i] - 1)
+        ub = y[i]
+        inverse[i] = brentq(
+            lambda x: massey_inequality(x) - y[i],
+            lb,
+            ub,
+            xtol=10**-5,
+            maxiter=100,
+            full_output=False,
+        )
+    return inverse
 
-    # Dichotomic search
-    for _ in range(niter):
-        x_mid = (x_lb + x_ub) / 2
-        x_lb, x_ub = np.where(f(x_mid) - y < 0, (x_mid, x_ub), (x_lb, x_mid))
 
-    if (abs(f(x_lb) - y) > 10**-2).any():
-        print("Inversion error exceeds 10^{-2} consider increasing niter")
-    return x_lb
+def guessing_entropy(
+    mutual_information: np.ndarray[float], key_size: int, base=2
+) -> np.ndarray[float]:
+    r"""Output a lower bound on the logarithm in base 2 of the guessing entropy
+    when a leakage upper bounded by the mutual information 'mutual_information' is disclosed to the adversary:
 
-
-def guessing_entropy(mutual_information, key_size, base=2):
-    r"""Output a lower bound on the logarithm in base 'base' of the guessing entropy
-    when a leakage upper bounded by the mutual information 'mutual_information' is disclosed to the adversary.
+    .. math::
+        \mathrm{G}(K|Y) = \log_2 \mathbb{E}[ \mathrm{rank}(K|Y)].
 
     Parameters
     ----------
@@ -138,7 +212,22 @@ def guessing_entropy(mutual_information, key_size, base=2):
     Returns
     -------
     Lower bound on the logarithm in  base 'base' of the guessing entropy
+
+    Examples
+    --------
+    >>> from scalib.postprocessing import guessing_entropy
+    >>> import numpy as np
+    >>> key_size=128
+    >>> mi = np.random.uniform(low=0, high=key_size, size=1)
+    >>> ge = guessing_entropy(mi, key_size)
     """
+    if not (mutual_information >= 0).all():
+        raise ValueError("Invalid inputs, the mutual information should be positive.")
+    if not (mutual_information * np.log2(base) <= key_size).all():
+        raise ValueError(
+            "Invalid inputs, the mutual information in bits should be less than the key_size."
+        )
+
     MI_nats = mutual_information * np.log(base)
     key_size_nats = key_size * np.log(2)
     entropy_nats = key_size_nats - MI_nats
@@ -152,12 +241,12 @@ def guessing_entropy(mutual_information, key_size, base=2):
         + np.log1p(np.exp(-key_size_nats))
         + np.log1p(-sqrt[mask])
     )
-    generic_bound = f_inv(entropy_nats)
-    bound = np.max([finite_size_bound, generic_bound], axis=0) / np.log(base)
+    generic_bound = massey_inequality_inverse(entropy_nats)
+    bound = np.max([finite_size_bound, generic_bound], axis=0) / np.log(2)  # in bits
     return bound
 
 
-def partial_sum_zeta(a, p, q):
+def partial_sum_zeta(a: np.ndarray[float], p: int, q: int) -> np.ndarray[float]:
     r"""Output the partial sum sum_{i=p}^q i^{-a}
 
     Parameters
@@ -174,7 +263,16 @@ def partial_sum_zeta(a, p, q):
     return np.sum(I ** -np.expand_dims(a, axis=0), axis=0)
 
 
-def euler_maclaurin_correction(a, k, log_M, order):
+def euler_maclaurin_correction(
+    a: np.ndarray[float], k: int, log_M: float, order: int
+) -> np.ndarray[float]:
+    r"""This sub-function computes the first "order" Euler-MacLaurin corrective terms needed to approximate the sum
+    sum_{i=k}^{exp(log_M)} i^{-a}.
+
+    These terms depends on both the Bernoulli numbers and the evalutation of the derivatives of i -> i^{-a}.
+    See  https://en.wikipedia.org/wiki/Euler%E2%80%93Maclaurin_formula
+
+    """
     if order == 0:
         return np.zeros_like(a)
     d_O = (np.arange(1, order + 1) * 2 - 1).reshape((order,) + (1,) * a.ndim)
@@ -186,8 +284,12 @@ def euler_maclaurin_correction(a, k, log_M, order):
     return np.sum((derivatives_M - derivatives_kp1) * EM_coeff, axis=0)
 
 
-def log_zeta_larger_than_1(a, log_M, order, k):
-
+def log_zeta_larger_than_1(
+    a: np.ndarray[float], log_M: float, order: int, k: int
+) -> np.ndarray[float]:
+    r"""This sub-function approximates the log of the truncated zeta function log(sum_{i=1}^{exp(log_M)} i^{-a})
+    for a parameter a > 1.
+    """
     # Partial Sum
     partial_sum = partial_sum_zeta(a, 1, k)
 
@@ -203,7 +305,12 @@ def log_zeta_larger_than_1(a, log_M, order, k):
     return np.log(partial_sum + integral + correction)
 
 
-def log_zeta_less_than_1(a, log_M, order, k):
+def log_zeta_less_than_1(
+    a: np.ndarray[float], log_M: float, order: int, k: int
+) -> np.ndarray[float]:
+    r"""This sub-function approximates the log of the truncated zeta function log(sum_{i=1}^{exp(log_M)} i^{-a})
+    for a parameter 0 < a < 1.
+    """
     log_M_nats = log_M * np.log(2)
     partial_sum = partial_sum_zeta(a, 1, k)
     correction = ((k + 1) ** (-a) + np.exp2(-a * log_M)) / 2
@@ -222,8 +329,12 @@ def log_zeta_less_than_1(a, log_M, order, k):
     )
 
 
-def log_zeta(a: np.ndarray[float], log_M: float, order=10, k=50):
-
+def log_zeta(
+    a: np.ndarray[float], log_M: float, order: int = 10, k: int = 50
+) -> np.ndarray[float]:
+    r"""This sub-function approximates the log of the truncated zeta function log(sum_{i=1}^{exp(log_M)} i^{-a}).
+    Depending on wether 0<a<1, a == 1 or a>1 it uses a specific expression taylored to ensure numerical stability.
+    """
     # Ensure that a is treated as a Numpy Array
     a = np.asarray(a)
 
@@ -260,9 +371,18 @@ def log_zeta(a: np.ndarray[float], log_M: float, order=10, k=50):
     return log_zeta_a
 
 
-def log_guessing_entropy(mutual_information, key_size, base=2, order=10, k=50):
-    r"""Output a lower bound on the logarithm in base 'base' of the log_guessing_entropy rank of the key of size key_size
-    when a leakage upper bounded by the mutual information 'mutual_information' is disclosed to the adversary.
+def log_guessing_entropy(
+    mutual_information: np.ndarray[float],
+    key_size: int,
+    base: int = 2,
+    order: int = 10,
+    k: int = 50,
+) -> np.ndarray[float]:
+    r"""Output a lower bound on the log-guessing entropy in base 2 of the rank of the key of size key_size
+    when a leakage upper bounded by the mutual information 'mutual_information' is disclosed to the adversary:
+
+    .. math::
+        \mathrm{LG}(K|Y) = \mathbb{E}[\log_2 \mathrm{rank}(K|Y)].
 
     Parameters
     ----------
@@ -279,7 +399,22 @@ def log_guessing_entropy(mutual_information, key_size, base=2, order=10, k=50):
     Returns
     -------
     Lower bound on the logarithm in  base 'base' of the log-guessing entropy rank of the key of size key_size when a leakage upper bounded by the mutual information 'mutual_information' is disclosed to the adversary.
+
+    Examples
+    --------
+    >>> from scalib.postprocessing import log_guessing_entropy
+    >>> import numpy as np
+    >>> key_size=128
+    >>> mi = np.random.uniform(low=0, high=key_size, size=1)
+    >>> lg = log_guessing_entropy(mi,key_size)
     """
+    if not (mutual_information >= 0).all():
+        raise ValueError("Invalid inputs, the mutual information should be positive.")
+    if not (mutual_information * np.log2(base) <= key_size).all():
+        raise ValueError(
+            "Invalid inputs, the mutual information in bits should be less than the key_size."
+        )
+
     mutual_information = np.atleast_1d(mutual_information)
 
     MI_nats = mutual_information * np.log(base)
@@ -293,14 +428,17 @@ def log_guessing_entropy(mutual_information, key_size, base=2, order=10, k=50):
         (np.expand_dims(entropy_nats, axis=1) - log_zeta(A, key_size, order, k)) / A,
         axis=1,
     )
-    return np.maximum(0, LG / np.log(base))
+    return np.maximum(0, LG / np.log(2))  # in bits
 
 
 def median(
     mutual_information: np.ndarray[float], key_size: int, base: float = 2
 ) -> np.ndarray[float]:
-    r"""Output a lower bound on the logarithm in base 'base' of the median rank of the key of size key_size
-    when a leakage upper bounded by the mutual information 'mutual_information' is disclosed to the adversary.
+    r"""Output a lower bound on the logarithm in base 2 of the median rank of the key of size key_size
+    when a leakage upper bounded by the mutual information 'mutual_information' is disclosed to the adversary:
+
+    .. math::
+        \log_2 \mathrm{Med}(K|Y) = \log_2 \min \{ m \mid \mathbb{P}( \mathrm{rank}(K|Y) \leq m) \geq 1/2 \}.
 
     Parameters
     ----------
@@ -314,18 +452,34 @@ def median(
     Returns
     -------
     Lower bound on the logarithm in  base 'base' of the median rank of the key of size key_size when a leakage upper bounded by the mutual information 'mutual_information' is disclosed to the adversary.
-    """
 
+    Examples
+    --------
+    >>> from scalib.postprocessing import median
+    >>> import numpy as np
+    >>> key_size=128
+    >>> mi = np.random.uniform(low=0, high=key_size, size=10**3)
+    >>> med = median(mi,key_size)
+    """
+    if not (mutual_information >= 0).all():
+        raise ValueError("Invalid inputs, the mutual information should be positive.")
+    if not (mutual_information * np.log2(base) <= key_size).all():
+        raise ValueError(
+            "Invalid inputs, the mutual information in bits should be less than the key_size."
+        )
+
+    med = np.zeros_like(mutual_information)
     # We use log1p and exp1m to have a stable expression
     # We deal with eventual overflows in the exponential for large mutual information next
-    median = (key_size - 1) / np.log2(base) + np.log1p(
-        -np.sqrt(-np.expm1(-2 * mutual_information * np.log(base)))
-    ) / np.log(base)
+    sqrt = np.sqrt(-np.expm1(-2 * mutual_information * np.log(base)))
+    med[sqrt < 1] = key_size - 1 + np.log1p(-sqrt[sqrt < 1]) / np.log(2)
 
     # To avoid numerical overflows we use an assymptotic expansion for large values of the mutual information
-    large_mi = mutual_information > 12
-    expansion_for_large_mi = np.maximum(
-        0, (key_size - 2) / np.log2(base) - 2 * mutual_information
+    # sqrt = 1 means that 1-exp(-2 * mutual_information * np.log(base)) is numerically simplified to 1 (large value of mutual_information)
+    # Hence 1-sqrt is zero and the log overflows.
+    # In this case we use the Taylor expansion log(1-sqrt(1-exp(-x))) = - (log 2) - x
+    med[sqrt == 1] = np.maximum(
+        0, key_size - 2 - 2 * mutual_information[sqrt == 1] * np.log2(base)
     )
 
-    return np.where(large_mi, expansion_for_large_mi, median)
+    return med
--- tests/test_leakage_to_success.py
+++ tests/test_leakage_to_success.py
@@ -10,9 +10,12 @@ from scalib.postprocessing import (
 
 
 def test_succss_rate():
+
+    rng = np.random.default_rng(seed=42)
+
     key_size = 128
 
-    mi = np.random.uniform(low=0, high=key_size, size=10**3)
+    mi = rng.uniform(low=0, high=key_size, size=10**3)
 
     # Check bound for typical key lenght
     sr_32 = success_rate(mi, key_size, enumeration_effort=32)
@@ -25,55 +28,64 @@ def test_succss_rate():
 
     # Check wether it works for large keys
     key_size = 1024
-    mi = np.random.uniform(low=0, high=key_size, size=10**3)
+    mi = rng.uniform(low=0, high=key_size, size=10**3)
     sr = success_rate(mi, key_size, enumeration_effort=32)
     assert (-key_size <= sr).all()
     assert (sr <= 0).all()
 
 
 def test_guessing_entropy():
+
+    rng = np.random.default_rng(seed=42)
+
     # Check bound for typical key length
     key_size = 128
-    mi = np.random.uniform(low=0, high=key_size, size=10**3)
+    mi = rng.uniform(low=0, high=key_size, size=10**3)
     ge = guessing_entropy(mi, key_size)
     assert (ge >= 0).all()
     assert (ge <= key_size).all()
 
     # Check wether it works for large keys
     key_size = 1024
-    mi = np.random.uniform(low=0, high=key_size, size=10**3)
+    mi = rng.uniform(low=0, high=key_size, size=10**3)
     ge = guessing_entropy(mi, key_size)
     assert (ge >= 0).all()
     assert (ge <= key_size).all()
 
 
 def test_log_guessing_entropy():
+
+    rng = np.random.default_rng(seed=42)
+
     # Check wether it works for typical key lenght
     key_size = 128
-    mi = np.random.uniform(low=0, high=key_size, size=10**3)
+    mi = rng.uniform(low=0, high=key_size, size=10**3)
     lg = log_guessing_entropy(mi, key_size)
     assert (lg >= 0).all()
     assert (lg <= key_size).all()
 
     # Check wether it works for large keys
     key_size = 1024
-    mi = np.random.uniform(low=0, high=key_size, size=10**3)
+    mi = rng.uniform(low=0, high=key_size, size=10**3)
     lg = log_guessing_entropy(mi, key_size)
     assert (lg >= 0).all()
     assert (lg <= key_size).all()
 
 
 def test_median():
+
+    rng = np.random.default_rng(seed=42)
+
     # Check wether it works for typical key lenght
     key_size = 128
-    mi = np.random.uniform(low=0, high=key_size, size=10**3)
+    mi = rng.uniform(low=0, high=key_size, size=10**3)
     med = median(mi, key_size)
     assert (med >= 0).all()
     assert (med <= key_size).all()
 
     # Check wether it works for large keys
     key_size = 1024
-    mi = np.random.uniform(low=0, high=key_size, size=10**3)
+    mi = rng.uniform(low=0, high=key_size, size=10**3)
     med = median(mi, key_size)
     assert (med >= 0).all()
     assert (med <= key_size).all()
